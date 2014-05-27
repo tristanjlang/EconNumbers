3dd9115e767c5c71393f1dcf81989c913e1d2b18
tristanjlang <tristan@alum.mit.edu>
2014-05-27 14:39:03 -0400

    write output to csv

--- yahoofinancedata.py
+++ yahoofinancedata.py
@@ -2,7 +2,7 @@
 Yahoo Finance puts economic data in html at: http://biz.yahoo.com/c/ec/200101.html
 URL format is: "<four digit year><week of year>.html"
 
-It is in an html table, so I should be able to just extract the data from the html.
+It is in an html table, so just extract the data from the html:
 1. economic data will be found in "soup.find_all('tr')", starting with element 6
 2. the data can be extracted by doing a ".find_all('td')" on each tr element found
 3. #2 will correspond with the following data:
@@ -17,21 +17,24 @@ It is in an html table, so I should be able to just extract the data from the ht
     -Revised
 4. repeat for all weeks of the year 01 through 53 (if applicable)
 5. repeat for all years from 2001 through 2014 (where applicable)
+6. write to csv so pandas can read it
 '''
 
 from bs4 import BeautifulSoup
 from urllib import request
 
-data = []
+def write_econ_data():
+    f = open('econdata.csv', 'w')
+    f.write('Year,Week,Date,Time (ET),Statistic,For,Actual,Briefing Forecast,Market Expects,Prior,Revised\n')
 
-for year in range(2001, 2015):
-    year = str(year)
-    for week in range(1, 54):
-        try:
-            week = str(week) if week > 9 else '0' + str(week)
-            r = request.urlopen('http://biz.yahoo.com/c/ec/' + year + week + '.html')
-            soup = BeautifulSoup(r.readall())
-            for tr in soup.find_all('tr')[6:]:
-                data.append([td.text for td in tr.find_all('td')])
-        except: pass
-print(data)
+    for year in range(2001, 2015):
+        year = str(year)
+        for week in range(1, 54):
+            try:
+                week = str(week) if week > 9 else '0' + str(week)
+                r = request.urlopen('http://biz.yahoo.com/c/ec/' + year + week + '.html')
+                soup = BeautifulSoup(r.readall())
+                for tr in soup.find_all('tr')[6:]:
+                    f.write(','.join([year, week] + [td.text for td in tr.find_all('td')]) + '\n')
+            except: pass
+    f.close()
