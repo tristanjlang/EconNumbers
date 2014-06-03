85afece837e3716906f5b9c277f2bf16cd537262
tristanjlang <tristan@alum.mit.edu>
2014-06-03 10:23:02 -0400

    fillna based on whether market expects or briefing forecast in NA

--- yahoofinancedata.py
+++ yahoofinancedata.py
@@ -22,6 +22,7 @@ It is in an html table, so just extract the data from the html:
 
 from bs4 import BeautifulSoup
 from urllib import request
+import numpy as np
 from numpy import nan as NA
 import pandas as pd
 import pandas.io.data as web
@@ -52,6 +53,8 @@ def mktdata(startdate='1/1/2001', enddate='12/31/2014'):
 
 
 def processframe(econdf):
+    NAs = [NA, 'nan', 'unch', 'Unch', 'no change', '-', '--', '---', 'DELAYED', 'DATE TBA', 'NA.']
+
     def processelement(element):
         if not isinstance(element, str) or ':' in element: return str(element)
         if len(element) > 2 and (element[0] == '$' or element[1] == '$'): element = element.replace('$','')
@@ -63,29 +66,26 @@ def processframe(econdf):
         elif element == '0.00%-0.25%' or element == '0-0.25%' or element == '0.00% -0.25%': return 0.25
         elif element[-2:] == '.%' or element[-2:] == '%%': return float(element[:-2])
         elif element[-1] == '%': return float(element[:-1]) if len(element) > 3 and element[-3] != ',' else float(element.replace(',','.')[:-1])
-        elif element == '-' or element == '--' or element == '---' or element == 'nan' or element == 'Unch' or element == 'unch' or element == 'no change' or element == 'DELAYED' or element == 'DATE TBA': return NA
-        elif element == 'ADP Employment' or element == 'ADP Employment Report': return 'ADP Employment Change'
-        elif element == 'Case Shiller 20 City Index' or element == 'Case-Shiller 20 City' or element == 'Case-Shiller 20-city Index (y/y)' or element == 'Case-Shiller Housing Price Index' or element == 'CaseShiller 20 City' or element == 'CaseShiller Home Price Index' or element == 'S&P;/Case-Shiller Home Price Index' or element == 'S&P;/CaseShiller Composite' or element == 'S&P;/CaseShiller Home Price Index': return 'Case-Shiller 20-city Index'
-        elif element == 'Core PCE Inflation' or element == 'PCE Prices' or element == 'Core PCE': return 'PCE Prices - Core'
+        elif element in NAs: return NA
+        elif element in ['ADP Employment', 'ADP Employment Report']: return 'ADP Employment Change'
+        elif element in ['Case Shiller 20 City Index', 'Case-Shiller 20 City', 'Case-Shiller 20-city Index (y/y)', 'Case-Shiller Housing Price Index', 'CaseShiller 20 City', 'CaseShiller Home Price Index', 'S&P;/Case-Shiller Home Price Index', 'S&P;/CaseShiller Composite', 'S&P;/CaseShiller Home Price Index']: return 'Case-Shiller 20-city Index'
+        elif element in ['Core PCE Inflation', 'PCE Prices', 'Core PCE']: return 'PCE Prices - Core'
         elif element == 'Current Account Balance': return 'Current Account'
         elif element == 'Durable Goods Orders': return 'Durable Orders'
-        elif element == 'Durable Goods - Ex Transportation' or element == 'Durable Goods -ex Transportation' or element == 'Durable Orders - ex Transportation' or element == 'Durable Orders - ex transporation' or element == 'Durable Orders -ex Auto' or element == 'Durable Orders -ex Transporation' or element == 'Durable Orders -ex Transportation' or element == 'Durable Orders ex Auto' or element == 'Durable Orders ex Transporation' or element == 'Durable Orders ex Transportation' or element == 'Durable Orders ex auto' or element == 'Durable Orders ex transportation' or element == 'Durable Orders, Ex-Auto' or element == 'Durable Orders, Ex-Tran' or element == 'Durable Orders, Ex-Transportation' or element == 'Durable Ordes ex Transportation' or element == 'Durables, Ex Transportation' or element == 'Durables, Ex-Tran' or element == 'Durables, Ex-Transport' or element == 'Durables, Ex-Transportation' or element == 'Durables, ex Transporation': return 'Durable Goods -ex transportation'
+        elif element in ['Durable Goods - Ex Transportation', 'Durable Goods -ex Transportation', 'Durable Orders - ex Transportation', 'Durable Orders - ex transporation', 'Durable Orders -ex Auto', 'Durable Orders -ex Transporation', 'Durable Orders -ex Transportation', 'Durable Orders ex Auto', 'Durable Orders ex Transporation', 'Durable Orders ex Transportation', 'Durable Orders ex auto', 'Durable Orders ex transportation', 'Durable Orders, Ex-Auto', 'Durable Orders, Ex-Tran', 'Durable Orders, Ex-Transportation', 'Durable Ordes ex Transportation', 'Durables, Ex Transportation', 'Durables, Ex-Tran', 'Durables, Ex-Transport', 'Durables, Ex-Transportation', 'Durables, ex Transporation']: return 'Durable Goods -ex transportation'
         elif element == 'Net Long-term TIC Flows': return 'Net Long-Term TIC Flows'
         elif element == 'Trsy Budget': return 'Treasury Budget'
         elif element == 'Unit Labor Costs - Preliminary': return 'Unit Labor Costs -Prel'
         elif element == 'Nonfarm Payrolls - Private': return 'Nonfarm Private Payrolls'
         elif element == 'NAHB Market Housing Index': return 'NAHB Housing Market Index'
         elif element == 'Mich Sentiment-Rev': return 'Mich Sentiment-Rev.'
-        elif element == 'FHFA Housing Price Index' or element == 'FHFA US Housing Price Index': return 'FHFA Home Price Index'
+        elif element in ['FHFA Housing Price Index', 'FHFA US Housing Price Index']: return 'FHFA Home Price Index'
         elif element == 'NAPM Index': return 'ISM Index'
         elif element == 'NAPM Services': return 'ISM Services'
-        elif element == 'NY Empire State Index': return 'Empire Manufacturing'
+        elif element in ['NY Empire State Index', 'Empire Manufacturing Index']: return 'Empire Manufacturing'
         else: return element
         
     def specialprocessrow(dfrow):
-        global stats
-        NAs = [NA, 'nan', 'unch', 'Unch', 'no change']
-
         if dfrow['Statistic'] in ['Trade Balance', 'Current Account', 'Current Account Balance', 'Consumer Credit', 'Treasury Budget', 'Trsy Budget']:
             if dfrow['Actual'] not in NAs and len(str(dfrow['Actual'])) > 1 and str(dfrow['Actual'])[-1] != 'B' and str(dfrow['Actual'])[-1] != 'M' and str(dfrow['Actual'])[-1] != '-': dfrow['Actual'] = str(dfrow['Actual']) + 'B'
             if dfrow['Briefing Forecast'] not in NAs and len(str(dfrow['Briefing Forecast'])) > 1 and str(dfrow['Briefing Forecast'])[-1] != 'B' and str(dfrow['Briefing Forecast'])[-1] != 'M' and str(dfrow['Briefing Forecast'])[-1] != '-': dfrow['Briefing Forecast'] = str(dfrow['Briefing Forecast']) + 'B'
@@ -111,6 +111,14 @@ def processframe(econdf):
         
         return dfrow
 
+    # *** MAJOR ASSUMPTION ***
+    # if one of briefing forecast or market expects is NA, fill value with the other
+    def myfillna(dfrow):
+        if dfrow['Briefing Forecast'] in NAs and dfrow['Market Expects'] in NAs: return dfrow
+        if dfrow['Briefing Forecast'] in NAs: dfrow['Briefing Forecast'] = float(dfrow['Market Expects'])
+        if dfrow['Market Expects'] in NAs: dfrow['Market Expects'] = float(dfrow['Briefing Forecast'])
+        return dfrow
+
     # additional helper functions
     '''
     NEED TO UPDATE NORMALIZE TO HANDLE NORMALIZE EACH COLUMN
@@ -123,6 +131,7 @@ def processframe(econdf):
     # apply helper functions
     df = econdf.apply(specialprocessrow, axis=1)
     df = df.applymap(processelement)
+    df = df.apply(myfillna, axis=1)
     df['close date before event'] = df.apply(getclosebeforedate, axis=1)
     df['open date after event'] = df.apply(getopenafterdate, axis=1)
     
@@ -137,6 +146,9 @@ def processframe(econdf):
     X = merged[['Actual', 'Briefing Forecast', 'Market Expects', 'Revised', 'Close_before', 'Adj Close_before']]
     y = merged[['Open_after', 'High_after', 'Low_after', 'Close_after', 'Adj Close_after']]
     X = X.applymap(lambda x: float(x) if isinstance(x, str) else x)
+    X['Statistic'] = merged['Statistic']
+    X['Date'] = merged['Date']
+    X['Year'] = merged['Year']
     
     # separate out the output values based on the close or the adjusted close before the event
     y_adj = y.copy()
@@ -157,31 +169,35 @@ def processframe(econdf):
     y_adj['Adj Close_after'] = y_adj['Adj Close_after'] / X['Adj Close_before']
     y_adj.columns = ['r_Open_after', 'r_High_after', 'r_Low_after', 'r_Close_after', 'r_Adj Close_after']
 
-    # remove the closes from the features
-    X = X[['Actual', 'Briefing Forecast', 'Market Expects', 'Revised']]
+    # convert inputs for the Statistics to be percent change from briefing forecast or market expects to actual
+    X['Pct Diff From Briefing Forecast'] = (X['Actual'] - X['Briefing Forecast']) / X['Briefing Forecast']
+    X['Pct Diff From Market Expects'] = (X['Actual'] - X['Market Expects']) / X['Market Expects']
+
+    # remove the closes and non-percent-change features from the features
+    X_brief = X[['Pct Diff From Briefing Forecast', 'Statistic', 'Date', 'Year']]
+    X_mkt = X[['Pct Diff From Market Expects', 'Statistic', 'Date', 'Year']]
+    X_brief = X_brief.replace([np.inf, -np.inf], np.nan).dropna()
+    X_mkt = X_mkt.replace([np.inf, -np.inf], np.nan).dropna()
+
     #X, y, y_adj = normalize(X), normalize(y), normalize(y_adj)
-    X = X.applymap(abs)
+    #X = X.applymap(abs)
     
-
-
-    X['Statistic'] = merged['Statistic']
-    X['Date'] = merged['Date']
-    X['Year'] = merged['Year']
-    return X, y, y_adj
+    return X_brief, X_mkt, y, y_adj
 
     
 #econdata(endyear=2002)
-X, y, y_adj = processframe(pd.read_table('econdata.tsv'))
+X_brief, X_mkt, y, y_adj = processframe(pd.read_table('econdata.tsv'))
 #print(X)
 #print(y)
 #print(y_adj)
-group = X.groupby(['Statistic'])[['Actual', 'Briefing Forecast', 'Market Expects', 'Revised']]
+group = X_brief.groupby(['Statistic'])[['Pct Diff From Briefing Forecast', 'Pct Diff From Market Expects']]
 for k, gp in group:
-        print('max = ' + str(gp.max()) + '\n')
-        print('min = ' + str(gp.min()) + '\n')
-        print('mean = ' + str(gp.mean()) + '\n')
-        print('median = ' + str(gp.median()) + '\n')
-        print('\n\n\n')
+    #print(k)
+    print('max = ' + str(gp.max()) + '\n')
+    print('min = ' + str(gp.min()) + '\n')
+    print('mean = ' + str(gp.mean()) + '\n')
+    print('median = ' + str(gp.median()) + '\n')
+    print('\n\n\n')
 #print(group.value_counts())
 '''for k, v in sorted(stats.items()):
     print(k, v)
@@ -189,4 +205,82 @@ X = X[(X['Statistic'] == 'Retail Sales') & (('%' not in X['Actual']) | ('%' not 
 #X = X[(X['Statistic'] == 'Retail Sales') & (('%' in X['Actual']) | ('%' in X['Briefing Forecast']) | ('%' in X['Market Expects']) | ('%' in X['Revised']))]
 print(X)'''
 
-#print(X[X['Statistic'] == 'Mich Sentiment- Final'])
+#print(X[X['Statistic'] == 'Help-Wanted Index'])
+
+
+
+
+##
+
+
+
+
+'''
+FULL LIST OF FEATURES TO ADD AS COLUMNS:
+
+
+ADP Employment Change
+Auto Sales
+Average Workweek
+Building Permits
+Business Inventories
+CPI
+Capacity Utilization
+Case-Shiller 20-city Index
+Chain Deflator-Adv.
+Chain Deflator-Final
+Chain Deflator-Prel.
+Chicago PMI
+Construction Spending
+Consumer Confidence
+Consumer Credit
+Continuing Claims
+Core CPI
+Core PPI
+Current Account
+Durable Goods -ex transportation
+Durable Orders
+Empire Manufacturing
+Employment Cost Index
+Existing Home Sales
+FHFA Home Price Index
+Factory Orders
+GDP Deflator
+GDP-Adv.
+GDP-Final
+GDP-Prel.
+Help-Wanted Index
+Hourly Earnings
+Housing Starts
+ISM Index
+ISM Services
+Industrial Production
+Initial Claims
+Leading Indicators
+Mich Sentiment- Final
+Mich Sentiment-Prel.
+Mich Sentiment-Rev.
+NAHB Housing Market Index
+Net Foreign Purchases
+Net Long-Term TIC Flows
+New Home Sales
+Nonfarm Payrolls
+Nonfarm Private Payrolls
+PCE Prices - Core
+PPI
+Pending Home Sales
+Personal Income
+Personal Spending
+Philadelphia Fed
+Productivity-Prel
+Productivity-Rev.
+Retail Sales
+Retail Sales ex-auto
+Trade Balance
+Treasury Budget
+Truck Sales
+Unemployment Rate
+Unit Labor Costs
+Unit Labor Costs - Rev
+Unit Labor Costs -Prel
+Wholesale Inventories'''
